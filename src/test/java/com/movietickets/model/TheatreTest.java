fd11b5fac4c6adab104cdeca3bbdf4fd2cbdc122
Yashwanth-Dev18 <yashwanthdev2005@gmail.com>
2025-12-14 20:13:53 +0100

    RED: Theatre Creation test case failed

---
+++ src/test/java/com/movietickets/model/TheatreTest.java
@@ -0,0 +1,15 @@
+package com.movietickets.model;
+
+import org.junit.jupiter.api.Test;
+
+public class TheatreTest {
+  
+  @Test
+  void testTheatreCreation() {
+    Theatre theatre = new Theatre(1, "Screen 1", 100);
+    assertEquals(1, theatre.getId());
+    assertEquals("Screen 1", theatre.getName());
+    assertEquals(100, theatre.getCapacity());
+  }
+  
+}
